ed1c001e9abd141fe7f7d147f72049cdfdf1abd6
adithya dinesh <adithyadinesh33@gmail.com>
2023-04-16 02:48:24 +0530

    Create SymmetricDifferenceOp.py

---
+++ SymmetricDifferenceOp.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+setn = set(map(int, input().split()))
+b = int(input())
+setb = set(map(int, input().split()))
+print(len(setb.symmetric_difference(setn)))
