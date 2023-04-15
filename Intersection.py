fdecda7343184d836c48383cef132b75ae2cd529
adithya dinesh <adithyadinesh33@gmail.com>
2023-04-16 02:47:11 +0530

    Create Intersection.py

---
+++ Intersection.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+setn = set(map(int, input().split()))
+b = int(input())
+setb = set(map(int, input().split()))
+setc = setn.intersection(setb)
+print(len(setc))
